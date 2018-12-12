0b9e81a189e7d16bd06649ce3edebb9f8d99ee4e
Nathaniel Fodrea <nathaniel.fodrea@genesys.com>
2018-12-12 11:49:42 -0500

    initial material UI commit

--- web/src/modules/user/LoginView.tsx
+++ web/src/modules/user/LoginView.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import Button from '@material-ui/core/Button';
+
 
 class LoginView extends React.PureComponent {
   render() {
     return (
       <div>
-        login view
-      </div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>      
+    </div>
     )
   }
 }
